7eb972d3d4e7727fefb3c74eedbcf169c6e18660
Sabri Dogru <github@mehmetcelebiturizm.com>
2026-08-27 08:49:04 +0300

    fix(models): correct BelongsToCompany trait namespace in LearnedStop model

--- app/Models/LearnedStop.php
+++ app/Models/LearnedStop.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\BelongsToCompany;
+use App\Models\Concerns\BelongsToCompany;
 
 class LearnedStop extends Model
 {
