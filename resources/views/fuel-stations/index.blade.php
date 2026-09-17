cf1b0fb59007946ad993aaadd73eb7fa08562265
Sabri Dogru <github@mehmetcelebiturizm.com>
2026-09-17 13:06:05 +0300

    Fix: Duzenle butonu Alpine.js veri aktarimi hatasi (quote escape) dataset ile cozuldu

--- resources/views/fuel-stations/index.blade.php
+++ resources/views/fuel-stations/index.blade.php
@@ -322,7 +322,8 @@
                                             </a>
                                             <button
                                                 type="button"
-                                                @click='openEditModal(@json($station))'
+                                                data-station="{{ $station->toJson() }}"
+                                                @click="openEditModal(JSON.parse($el.dataset.station))"
                                                 class="rounded-xl border border-slate-200 bg-white px-3 py-2 text-xs font-semibold text-blue-600 transition hover:bg-blue-50 hover:border-blue-200"
                                             >
                                                 Düzenle
